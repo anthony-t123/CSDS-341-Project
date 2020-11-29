40b631458dbf5490fc0852dcaffa0684d5d50514
anthony-t123 <anthony.n.testa@gmail.com>
2020-11-29 16:17:05 -0500

    Added other methods for embedded SQL queries

--- Code/Python/queries.py
+++ Code/Python/queries.py
@@ -18,3 +18,12 @@ def state_color(year):
 
 def states():
     return execute_query('SELECT StateName FROM States')
+
+
+def state_votes(year, candidate):
+    #Return number of votes in states for given year and candidate
+    return None
+
+def state_electoral(year):
+    #Return number of electoral votes states have in a given year
+    return None
